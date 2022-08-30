fa5c5602b19dc41f9347c4e12a5f40e9dbbf4393
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-08-30 17:04:09 +0500

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,6 +6,24 @@ import java.io.File;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> personalData = new HashMap<>();
+        Profile profile = new Profile();
+        String data;
+
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            while ((data = reader.readLine()) != null) {
+                String[] keyValue = data.split(":");
+                personalData.put(keyValue[0].trim(), keyValue[1].trim());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        profile.setName(personalData.get("Name"));
+        profile.setAge(Integer.parseInt(personalData.get("Age")));
+        profile.setEmail(personalData.get("Email"));
+        profile.setPhone(Long.parseLong(personalData.get("Phone")));
+
+        return profile;
     }
 }
