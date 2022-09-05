49cea0b024be3b984766a5976f3a48216f085750
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2022-09-05 11:17:01 +0500

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -12,14 +12,28 @@ public class FileReader {
         Profile profile = new Profile();
         String data;
 
-        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
-            while ((data = reader.readLine()) != null) {
-                String[] keyValue = data.split(":");
-                personalData.put(keyValue[0].trim(), keyValue[1].trim());
+         try (InputStream in = new FileInputStream("Profile.txt")) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                while ((data = reader.readLine()) != null){
+                    String[] keyValue = data.split(":");
+                    personalData.put(keyValue[0].trim(), keyValue[1].trim());
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+
+//2 todo second way
+//            try(BufferedReader reader = new BufferedReader(new java.io.FileReader("Profile.txt"))) {
+//                while ((data = reader.readLine()) != null){
+//                    String[] keyValue = data.split(":");
+//                    personalData.put(keyValue[0].trim(), keyValue[1].trim());
+//                }
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
 
         profile.setName(personalData.get("Name"));
         profile.setAge(Integer.parseInt(personalData.get("Age")));
